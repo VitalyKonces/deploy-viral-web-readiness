bc1ddb3beab6b01d9f1d3370b84488350ef85eeb
VitalyKonces <140142399+VitalyKonces@users.noreply.github.com>
2023-10-05 14:54:25 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,40 +1,27 @@
-
-        print(f"Random Number: {item}")
-def generate_random_data():
+    data = generate_random_data()
 def main():
     return data
-import random
-
+    data = [random.randint(1, 100) for _ in range(10)]
     main()
-
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
 
-    for item in data:    main()
 
 def generate_random_data():
-def main():
-
-    return data
-    for item in data:
 import random
-if __name__ == "__main__":
-
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
         print(f"Random Number: {item}")
 
-import random
+
     return data
-def main():
+def generate_random_data():
+if __name__ == "__main__":
 
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
     for item in data:
-    data = generate_random_data()
-
     main()
+    data = generate_random_data()
 
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-def generate_random_data():
         print(f"Random Number: {item}")
+def main():
